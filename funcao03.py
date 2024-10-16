45d34373f8781f524510961d1592a2292cdef2d4
NaterciaMM <naterciamm2020@gmail.com>
2024-10-16 14:05:56 -0300

    ttt

---
+++ funcao03.py
@@ -0,0 +1,17 @@
+# Crie duas funções:
+# - calcular_area_base, que recebe o raio de um círculo e retorna sua área.
+# - calcular_volume_cilindro, que utiliza a função calcular_area_base para calcular o volume de um cilindro dado o raio e a altur
+
+raio = int(input("digite o raio: "))
+haltura = int(input("digite a altura: "))
+def calcular_area_base():
+    raioreult = 2 * 3.14 * raio
+
+def calcular_volume_cilindro ():
+    volume = calcular_area_base()^2* 3,14 * haltura
+
+raioprint =  calcular_area_base()
+volumeprint = calcular_volume_cilindro ()
+
+print (raioprint)
+print (volumeprint)
